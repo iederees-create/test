4a54ad654656e4fde0b9c5195db0ae8a54b5cb2d
Iederees Francis <63100527+iederees-create@users.noreply.github.com>
2025-07-16 10:00:11 +0200

    Create script.js

---
+++ script.js
@@ -0,0 +1,7 @@
+// Currently no JavaScript functionality is visible in the screenshot
+// This file is included for future expansion
+
+document.addEventListener('DOMContentLoaded', function() {
+    // Any future JavaScript can go here
+    console.log('RAVERSUS website loaded');
+});
